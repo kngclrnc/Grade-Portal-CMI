d67361bd9afcb5e3ae39d497137ec3284fd51573
kngclrnc <51502199+kngclrnc@users.noreply.github.com>
2021-11-17 15:34:29 +0800

    Update newStudent.php

--- kulot/newStudent.php
+++ kulot/newStudent.php
@@ -1,21 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "cmi_gradedb";
+include "function.php";
+include "connection.php";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$db);
-$course_id = $_POST["course_id"];
-$student_number = $_POST["student_number"];
-$first_name = $_POST["first_name"];
-$mid_name =  $_POST["mid_name"];
-$last_name =  $_POST["last_name"];
-$ext_name =  $_POST["ext_name"];
-$contact_number =  $_POST["contact_number"];
-$email_address =  $_POST["email_address"];
-$username =  $_POST["username"];
-$password =  md5($_POST["password"]);
+
+$course_id = clean_string($_POST["course_id"]);
+$student_number = clean_string($_POST["student_number"]);
+$first_name = clean_string($_POST["first_name"]);
+$mid_name =  clean_string($_POST["mid_name"]);
+$last_name =  clean_string($_POST["last_name"]);
+$ext_name =  clean_string($_POST["ext_name"]);
+$contact_number =  clean_string($_POST["contact_number"]);
+$email_address =  clean_string($_POST["email_address"]);
+$username =  clean_string($_POST["username"]);
+$password =  clean_string(md5($_POST["password"]));
 $role =  "STUDENT";
 $new_password = $password;
 

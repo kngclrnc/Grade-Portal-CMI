2a1f4357b5168e317155beed6b7bbad191ff6376
kngclrnc <51502199+kngclrnc@users.noreply.github.com>
2021-11-17 15:35:01 +0800

    Update profpage.php

--- kulot/profpage.php
+++ kulot/profpage.php
@@ -1,8 +1,6 @@
 <?php 
-	session_start();
-	$username = $_SESSION['username'];
-	$userFN = $_SESSION['first_name'];
-	$userLN = $_SESSION['last_name'];
+	include "function.php";
+
 ?>
 
 <!DOCTYPE html>
@@ -22,354 +20,65 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="css/page.css?v=<?php echo time();?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
 </head>
-<style>
 
-    body {
-        color: #566787;
-		background: #f5f5f5;
-		font-family: 'Varela Round', sans-serif;
-		font-size: 13px;
-	}
-	.table-responsive {
-        margin: 30px 0;
-    }
-	.table-wrapper {
-		min-width: 1000px;
-        background: #fff;
-        padding: 20px 25px;
-		border-radius: 3px;
-        box-shadow: 0 1px 1px rgba(0,0,0,.05);
-    }
-	.table-title {        
-		padding-bottom: 15px;
-		background: #435d7d;
-		color: #fff;
-		padding: 16px 30px;
-		margin: -20px -25px 10px;
-		border-radius: 3px 3px 0 0;
-    }
-    .table-title h2 {
-		margin: 5px 0 0;
-		font-size: 24px;
-	}
-	.table-title .btn-group {
-		float: right;
-	}
-	.table-title .btn {
-		color: #fff;
-		float: right;
-		font-size: 13px;
-		border: none;
-		min-width: 50px;
-		border-radius: 2px;
-		border: none;
-		outline: none !important;
-		margin-left: 10px;
-	}
-	.table-title .btn i {
-		float: left;
-		font-size: 21px;
-		margin-right: 5px;
-	}
-	.table-title .btn span {
-		float: left;
-		margin-top: 2px;
-	}
-    table.table tr th, table.table tr td {
-        border-color: #e9e9e9;
-		padding: 12px 15px;
-		vertical-align: middle;
-    }
-	table.table tr th:first-child {
-		width: 60px;
-	}
-	table.table tr th:last-child {
-		width: 100px;
-	}
-    table.table-striped tbody tr:nth-of-type(odd) {
-    	background-color: #fcfcfc;
-	}
-	table.table-striped.table-hover tbody tr:hover {
-		background: #f5f5f5;
-	}
-    table.table th i {
-        font-size: 13px;
-        margin: 0 5px;
-        cursor: pointer;
-    }	
-    table.table td:last-child i {
-		opacity: 0.9;
-		font-size: 22px;
-        margin: 0 5px;
-    }
-	table.table td a {
-		font-weight: bold;
-		color: #566787;
-		display: inline-block;
-		text-decoration: none;
-		outline: none !important;
-	}
-	table.table td a:hover {
-		color: #2196F3;
-	}
-	table.table td a.edit {
-        color: #FFC107;
-    }
-    table.table td a.delete {
-        color: #F44336;
-    }
-    table.table td i {
-        font-size: 19px;
-    }
-	table.table .avatar {
-		border-radius: 50%;
-		vertical-align: middle;
-		margin-right: 10px;
-	}
-    .pagination {
-        float: right;
-        margin: 0 0 5px;
-    }
-    .pagination li a {
-        border: none;
-        font-size: 13px;
-        min-width: 30px;
-        min-height: 30px;
-        color: #999;
-        margin: 0 2px;
-        line-height: 30px;
-        border-radius: 2px !important;
-        text-align: center;
-        padding: 0 6px;
-    }
-    .pagination li a:hover {
-        color: #666;
-    }	
-    .pagination li.active a, .pagination li.active a.page-link {
-        background: #03A9F4;
-    }
-    .pagination li.active a:hover {        
-        background: #0397d6;
-    }
-	.pagination li.disabled i {
-        color: #ccc;
-    }
-    .pagination li i {
-        font-size: 16px;
-        padding-top: 6px
-    }
-    .hint-text {
-        float: left;
-        margin-top: 10px;
-        font-size: 13px;
-    }    
-	/* Custom checkbox */
-	.custom-checkbox {
-		position: relative;
-	}
-	.custom-checkbox input[type="checkbox"] {    
-		opacity: 0;
-		position: absolute;
-		margin: 5px 0 0 3px;
-		z-index: 9;
-	}
-	.custom-checkbox label:before{
-		width: 18px;
-		height: 18px;
-	}
-	.custom-checkbox label:before {
-		content: '';
-		margin-right: 10px;
-		display: inline-block;
-		vertical-align: text-top;
-		background: white;
-		border: 1px solid #bbb;
-		border-radius: 2px;
-		box-sizing: border-box;
-		z-index: 2;
-	}
-	.custom-checkbox input[type="checkbox"]:checked + label:after {
-		content: '';
-		position: absolute;
-		left: 6px;
-		top: 3px;
-		width: 6px;
-		height: 11px;
-		border: solid #000;
-		border-width: 0 3px 3px 0;
-		transform: inherit;
-		z-index: 3;
-		transform: rotateZ(45deg);
-	}
-	.custom-checkbox input[type="checkbox"]:checked + label:before {
-		border-color: #03A9F4;
-		background: #03A9F4;
-	}
-	.custom-checkbox input[type="checkbox"]:checked + label:after {
-		border-color: #fff;
-	}
-	.custom-checkbox input[type="checkbox"]:disabled + label:before {
-		color: #b8b8b8;
-		cursor: auto;
-		box-shadow: none;
-		background: #ddd;
-	}
-	/* Modal styles */
-	.modal .modal-dialog {
-		max-width: 400px;
-	}
-	.modal .modal-header, .modal .modal-body, .modal .modal-footer {
-		padding: 20px 30px;
-	}
-	.modal .modal-content {
-		border-radius: 3px;
-	}
-	.modal .modal-footer {
-		background: #ecf0f1;
-		border-radius: 0 0 3px 3px;
-	}
-    .modal .modal-title {
-        display: inline-block;
-    }
-	.modal .form-control {
-		border-radius: 2px;
-		box-shadow: none;
-		border-color: #dddddd;
-	}
-	.modal textarea.form-control {
-		resize: vertical;
-	}
-	.modal .btn {
-		border-radius: 2px;
-		min-width: 100px;
-	}	
-	.modal form label {
-		font-weight: normal;
-	}	
-
-    a {
-        color: white;
-    }
-</style>
 <script>
     $(document).ready(function(){
-	// Activate tooltip
-	$('[data-toggle="tooltip"]').tooltip();
-	
-	// Select/Deselect checkboxes
-	var checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function(){
-		if(this.checked){
-			checkbox.each(function(){
-				this.checked = true;                        
-			});
-		} else{
-			checkbox.each(function(){
-				this.checked = false;                        
-			});
-		} 
+		// Activate tooltip
+		$('[data-toggle="tooltip"]').tooltip();
+		$('#data_table').DataTable();
+		// Select/Deselect checkboxes
+		var checkbox = $('table tbody input[type="checkbox"]');
+		$("#selectAll").click(function(){
+			if(this.checked){
+				checkbox.each(function(){
+					this.checked = true;                        
+				});
+			} else{
+				checkbox.each(function(){
+					this.checked = false;                        
+				});
+			} 
+		});
+		checkbox.click(function(){
+			if(!this.checked){
+				$("#selectAll").prop("checked", false);
+			}
+		});
 	});
-	checkbox.click(function(){
-		if(!this.checked){
-			$("#selectAll").prop("checked", false);
-		}
-	});
-});
 </script>
 <body>
-    
-    <div class="wrapper">
+
+	<div class="wrapper">
     <!-- Sidebar  -->
-    <nav id="sidebar">
-        <div class="sidebar-header">
-		<span><img src="image/logo.png" style="margin-left: 65px;" class="rounded-circle" alt="Cinque Terre" width="70px" height="70px"></span>
-        <span><h3>College of Mary Immaculate</h3></span>
-        </div>
-
-        <ul class="list-unstyled components">
-       
-        <span><img src="image/Profile-Avatar-PNG.png" style="margin-left: 30px;" class="rounded-circle" alt="Cinque Terre" width="50px" height="50px"></span>
-        <span><?php echo "{$userFN} {$userLN}"; ?></span> 
-        <br>
-        <b style="font-size: 20px; margin-left: 75px;"> Student No: 1800123</b>
-        <br>
-        <br>
-        
-        <li>
-            <a href="profpage.php">My Classes </a>
-        </li>
-       
-        <li>
-            <a href="#">My Account	</a>
-        </li>
-
-        <li>
-            <a href="#">Contact</a>
-        </li>
-        </ul>
-
-        <ul class="list-unstyled CTAs">
-        <li>
-            <a href="index.php" class="logout">Log out</a> 
-        </li>
-        <li>
-            <a href="#" class="About_us">About us</a>
-        </li>
-        </ul>
-    </nav>
+    <?php system_navbar(); ?>
+    
+  
 
     <!-- Page Content  -->
     <div id="content">
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-
-            <button type="button" id="sidebarCollapse" class="btn btn-info">
-            <span>Toggle Sidebar</span>
-            </button>
-            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-align-justify"></i>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item active">
-                <a class="nav-link" href="#">Page</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Page</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Page</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Page</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-        </nav>
+		<?php toggle_navbar('PAGE1', 'PAGE2', 'PAGE3', 'PAGE4'); ?>
         <!-- content -->
-       
-        <div class="container">
-		<div class="table-responsive">
-			<div class="table-wrapper">
-				<div class="table-title">
-					<div class="row">
-						<div class="col-xs-6">
-							<h2>Handled <b>Subjects</b></h2>
-						</div>
-						<div class="col-xs-6">
-							<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New User</span></a>
-							<a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
+
+		<div class="container-fluid">
+			<div class="table-responsive">
+				<div class="table-wrapper">
+					<div class="table-title">
+						<div class="row">
+							<div class="col-xs-6">
+								<h2><b> Manage My Account</b></h2>
+							</div>
 						</div>
 					</div>
-				</div>
-				<table class="table table-striped table-hover">
-					<thead>
+					<table class="table table-striped table-hover" id="data_table">
+						<thead>
 
 					<!-- dito kau mga eedit sa table -->
 						<tr>
@@ -387,8 +96,7 @@
 					</thead>
 					<tbody>
 					<?php
-						$conn = mysqli_connect("localhost", "root", "", "cmi_gradedb");
-						// Check connection
+						include "connection.php";
 						if ($conn->connect_error) {
 						die("Connection failed: " . $conn->connect_error);
 						}
@@ -481,12 +189,6 @@
 							<label>Password</label>
 							<input type="password" name="password" class="form-control" required>
 						</div>				
-						<div class="form-group">
-							<select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" name="role">
-							<option value="admin">Admin</option>	
-							<option value="professor">Professor</option>
-							</select>
-						</div>						
 					</div>
 					<div class="modal-footer">
 						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">

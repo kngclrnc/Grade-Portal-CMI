5f1121fba9b220a3dd427576742bddc07c0e1069
kngclrnc <51502199+kngclrnc@users.noreply.github.com>
2021-11-17 15:32:24 +0800

    Update logout.php

--- kulot/logout.php
+++ kulot/logout.php
@@ -1,4 +1,19 @@
 <?php
+    include "connection.php";
+    session_start();
+    $id = $_SESSION['id'];
+    $role = $_SESSION['role'];
+    if ($role == "PROFESSOR") {
+
+        $query = mysqli_query($conn, 'UPDATE `prof_tb` SET `is_login` = 0 WHERE id = '.$id);
+    }
+    elseif ($role == "ADMIN") {
+        $query = mysqli_query($conn, 'UPDATE `admins_tb` SET `is_login` = 0 WHERE id = '.$id);
+    }
+    elseif ($role == "STUDENT") {
+        $query = mysqli_query($conn, 'UPDATE `student_tb` SET `is_login` = 0 WHERE id = '.$id);
+    }
+
 session_destroy();
 header("Location: /kulot/index.php");
 

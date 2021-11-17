e24a1c18b29528ae113aef4f364ec3bbc4304d91
kngclrnc <51502199+kngclrnc@users.noreply.github.com>
2021-11-17 15:32:03 +0800

    Update login.php

--- kulot/login.php
+++ kulot/login.php
@@ -1,40 +1,7 @@
 <?php
-    function getvalue($idx)
-    {
-        if(isset($_POST[$idx]))
-        {
-            return $_POST[$idx];
-        }
-        else if (isset($_GET[$idx]))
-        {
-            return $_GET[$idx];
-        }
-        else
-        {
-            return '';
-        }
-    }
-
-    function clean_string($str)
-    {
-        include ("connection.php");
-        return mysqli_real_escape_string($conn,$str);
-    }
-
-    function select($table, $where)
-    {
-        include ("connection.php");
-        $sql = "SELECT * FROM `$table` $where";
-        $result = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($result) > 0)
-        {
-            return $result;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    session_start();
+    include "function.php";
+ 
 
 
     include ("connection.php");
@@ -48,53 +15,43 @@
         $result = select('admins_tb', $where);
         $result1 = select('student_tb', $where);
         $result2 = select('prof_tb', $where);
-        if($result)
-        {
-            $row = mysqli_fetch_array($result);    
-            session_start();
-            $_SESSION['first_name'] = $row['first_name'];
-            $_SESSION['last_name'] = $row['last_name'];
-            $_SESSION['role'] = $row['role'];
-            $_SESSION['is_login'] = $row['is_login'];
-            $_SESSION['id'] = $row['id'];
-            $query = mysqli_query($conn, 'UPDATE `admins_tb` SET `is_login` = 1 WHERE id = '.$_SESSION['id']);
-            if ($row['role'] == 'ADMIN') {
-                echo"<script type='text/javascript'>alert('Welcome to Admin page!');window.location.href='/kulot/adminpage.php';</script>";
-            }
+        $admin = mysqli_fetch_array($result);    
+        $student = mysqli_fetch_array($result1);    
+        $prof = mysqli_fetch_array($result2);    
+        
 
+        if($result)
+        {   
+            $query = mysqli_query($conn, 'UPDATE `admins_tb` SET `is_login` = 1 WHERE id = '.$admin['id']);
+            if ($admin['role'] == 'ADMIN') {
+                $_SESSION['id'] = $admin['id'];
+                $_SESSION['role'] = $admin['role'];
+                // echo"<script type='text/javascript'>alert('Welcome to Admin page!');window.location.href='/kulot/adminpage.php';</script>";
+                header("Location: /kulot/adminpage.php");
+            }
             
             
         }
         elseif ($result1)
         {
-            
-            $row = mysqli_fetch_array($result1);    
-            session_start();
-            $_SESSION['first_name'] = $row['first_name'];
-            $_SESSION['last_name'] = $row['last_name'];
-            $_SESSION['role'] = $row['role'];
-            $_SESSION['is_login'] = $row['is_login'];
-            $_SESSION['id'] = $row['id'];
-            $query = mysqli_query($conn, 'UPDATE `student_tb` SET `is_login` = 1 WHERE id = '.$_SESSION['id']);
-            if ($row['role'] == 'STUDENT') {
-                echo"<script type='text/javascript'>alert('Welcome to Student grade portal!');window.location.href='/kulot/studentpage.php';</script>";
+            $query = mysqli_query($conn, 'UPDATE `student_tb` SET `is_login` = 1 WHERE id = '.$prof['id']);
+            if ($student['role'] == 'STUDENT') {
+                $_SESSION['id'] = $student['id'];
+                $_SESSION['role'] = $student['role'];
+                header("Location: /kulot/studentpage.php");
             }
         }
         elseif ($result2) {
-            $row = mysqli_fetch_array($result2);    
-            session_start();
-            $_SESSION['first_name'] = $row['first_name'];
-            $_SESSION['last_name'] = $row['last_name'];
-            $_SESSION['role'] = $row['role'];
-            $_SESSION['is_login'] = $row['is_login'];
-            $_SESSION['id'] = $row['id'];
-            $query = mysqli_query($conn, 'UPDATE `prof_tb` SET `is_login` = 1 WHERE id = '.$_SESSION['id']);
-            if ($row['role'] == 'PROFESSOR') {
-                echo"<script type='text/javascript'>alert('Welcome Professor!');window.location.href='/kulot/profpage.php';</script>";
+            $query = mysqli_query($conn, 'UPDATE `prof_tb` SET `is_login` = 1 WHERE id = '.$student['id']);
+            if ($prof['role'] == 'PROFESSOR') {
+                $_SESSION['id'] = $prof['id'];
+                $_SESSION['role'] = $prof['role'];
+                header("Location: /kulot/profpage.php");
             }
-            else{
-                echo "Invalid Password";
-            }
+        }
+        else {
+            header("Location: /kulot/index.php");
+            
         }
     }
 ?>

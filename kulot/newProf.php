ddb051f5788a95b0d7a74810a6b7caf79d50a0b7
kngclrnc <51502199+kngclrnc@users.noreply.github.com>
2021-11-17 15:34:14 +0800

    Update newProf.php

--- kulot/newProf.php
+++ kulot/newProf.php
@@ -1,15 +1,16 @@
 <?php
-    include ("connection.php");
+include "function.php";
+include "connection.php";
 
 
-$first_name = $_POST["first_name"];
-$mid_name = $_POST["mid_name"];
-$last_name =  $_POST["last_name"];
-$ext_name =  $_POST["ext_name"];
-$contact_number = $_POST["contact_number"];
-$email_address = $_POST["email_address"]; 
-$username = $_POST["username"];
-$password =  md5($_POST["password"]);
+$first_name = clean_string($_POST["first_name"]);
+$mid_name = clean_string($_POST["mid_name"]);
+$last_name =  clean_string($_POST["last_name"]);
+$ext_name =  clean_string($_POST["ext_name"]);
+$contact_number = clean_string($_POST["contact_number"]);
+$email_address = clean_string($_POST["email_address"]); 
+$username = clean_string($_POST["username"]);
+$password =  clean_string(md5($_POST["password"]));
 $role =  "PROFESSOR";
 
 if (isset($_REQUEST["addProf"])) {
